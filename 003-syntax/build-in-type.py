ce8cb15f187c5aeb394f7b56a01aba0f247d5e46
Chong Chen <chongchen1999@gmail.com>
2024-11-27 17:27:25 -0500

    update

---
+++ 003-syntax/build-in-type.py
@@ -0,0 +1,14 @@
+# variables in python have no data type, 
+# they are just references to objects
+
+a = 123
+b = 3.14
+c = True
+d = "Hello World"
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+print("b: " + str(b))
