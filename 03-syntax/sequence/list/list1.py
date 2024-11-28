46219bf614e12475c1b1d4f5eb4a4191594871b3
Chong Chen <chongchen1999@gmail.com>
2024-11-28 12:52:33 -0500

    update

---
+++ 03-syntax/sequence/list/list1.py
@@ -0,0 +1,14 @@
+def list_basic():
+    w = range(1, 10)
+    a = list(range(1, 10))
+    b = list(range(3, 15, 3))
+
+    print(w)
+    print(a)
+    print(b)
+
+    print(type(w), type(a), type(b))
+
+
+a = [x * 2 for x in range(20) if x % 3 == 1]
+print(a)
