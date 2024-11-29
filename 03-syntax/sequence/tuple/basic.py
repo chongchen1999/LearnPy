7427d2359afe375b6c68ec5efc2de27a83eae5d7
Chong Chen <chongchen1999@gmail.com>
2024-11-29 11:47:37 -0500

    update

--- 03-syntax/sequence/tuple/basic.py
+++ 03-syntax/sequence/tuple/basic.py
@@ -2,3 +2,8 @@ a = (1, 2, 3, )
 print(a)
 print(type(a))
 print(len(a))
+
+
+a = ["aa", 1, 2]
+print(a)
+print(type(a))
---
+++ 03-syntax/sequence/tuple/zip.py
@@ -0,0 +1,10 @@
+a = list(range(5))
+b = [6, 7, 8]
+
+print(a)
+print(b)
+
+c = zip(a, b)
+print(c)
+print(hex(id(c)))
+
