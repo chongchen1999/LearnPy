7a9b2fd49879468f312cefccb64b9a0de97e3235
Chong Chen <chongchen1999@gmail.com>
2024-12-15 15:35:51 -0500

    update

--- 08-file-io/example.py
+++ 08-file-io/example.py
@@ -1,3 +1,6 @@
-with open("data/example.txt", "w") as f:
+with open("data/example.txt", "a") as f:
     f.write("This is a test!\n")
 
+with open("data/example.txt", "r") as f:
+    for s in f.readlines():
+        print(s, end = "")
