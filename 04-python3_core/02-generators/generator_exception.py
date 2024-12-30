bfe5b669e137c2bcd5d7af85abfa553028edc459
Chong Chen <chongchen1999@gmail.com>
2024-12-30 15:06:11 -0500

    update

---
+++ 04-python3_core/02-generators/generator_exception.py
@@ -0,0 +1,22 @@
+def fib_generator():
+    a, b = 0, 1
+    yield a
+
+    b, a = a + b, b
+    yield a
+
+    b, a = a + b, b
+    yield a
+
+    b, a = a + b, b
+    yield a
+
+    print('This is the last line of the generator function')
+    return 'Done'
+
+gen = fib_generator()
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
