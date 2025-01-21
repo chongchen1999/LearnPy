70f2e29164404e0da2c2fcc459642abe0c26a8eb
Chong Chen <chongchen1999@gmail.com>
2025-01-20 23:25:31 -0500

    update keep last n elements

---
+++ 04-py_cookbook/ch1-data_structure_and_algoriothms/1.3-keep_last_n_elements.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+def search(lines, pattern, history=4):
+    previous_lines = deque(maxlen=history)
+    for line in lines:
+        if pattern in line:
+            yield line, previous_lines
+        previous_lines.append(line)
+
+# Example use on a file
+if __name__ == '__main__':
+    # Writing content to somefile.txt
+    with open('data/somefile.txt', 'w') as f:
+        for i in range(10):
+            f.write("This is line %d\n" % (i+1))
+
+    with open('data/somefile.txt') as f:
+        search_result = search(f, 'line', 3)
+        print(search_result)
+        for line, prevlines in search_result:
+            for pline in prevlines:
+                print("Previous line: ", pline, end='')
+            print("This line: ", line, end='')
+            print('-'*20)
